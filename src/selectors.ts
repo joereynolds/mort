6d229c70d16a3f9f946285112f34881b60c45e2a
Joe Reynolds <joereynolds952@gmail.com>
2018-04-08 04:29:09 +0100

    use constants in selectors.ts

--- src/selectors.ts
+++ src/selectors.ts
@@ -2,11 +2,25 @@ const fs = require("fs");
 
 class Selectors {
 
+    private readonly id: string = "#";
+    private readonly class: string = ".";
+
+    private readonly pseudoSelectors: string[] = [
+        ":checked",
+        ":disabled",
+        ":focus",
+        ":hover",
+        ":invalid",
+        ":read-only",
+        ":required",
+        ":valid",
+    ];
+
     public fromFile(file: string): string[] {
         const fileContents: string = fs.readFileSync(file, "utf8");
 
         const selectors = fileContents.split("\n").filter(selector => {
-            return (selector.startsWith("#") || selector.startsWith("."));
+            return (selector.startsWith(this.id) || selector.startsWith(this.class));
         });
 
         return selectors;
