93e8e61f927bb1ef9f6630e7a1883b139e184d1b
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2018-04-17 14:43:15 +0100

    actually add grep

---
+++ src/grep-programs/grep.ts
@@ -0,0 +1,38 @@
+const child_process = require("child_process");
+import { IGrep } from "../interfaces/IGrep";
+import { Selectors } from "../selectors";
+
+class Grep implements IGrep {
+
+    public readonly executable: string = "grep";
+    public readonly ignoreCase: string = "-i";
+    public readonly filesToIgnore: string = "--exclude=*.css";
+
+    public run(cssFilePath: string, searchOnly: string = "."): string[]  {
+        const selectors = new Selectors();
+        const cleanSelectors = selectors.clean(selectors.fromFile(cssFilePath));
+        return selectors.findUsages(this, searchOnly, cleanSelectors);
+    }
+
+    public call(selector: string, path: string) {
+        const call = child_process.spawnSync(
+            this.executable,
+            [
+                "-r",
+                this.ignoreCase,
+                this.filesToIgnore,
+                "--exclude=*.scss", // Again, couldn't get this working in the same string
+                selector,
+                path,
+            ],
+            {
+                stdio: "pipe",
+                encoding: "utf-8",
+            },
+        );
+
+        return call;
+    }
+}
+
+export { Grep };
