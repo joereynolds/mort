8eaabb661a405bce8695d2707f2cd28a27b36f1c
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2018-05-01 15:19:38 +0100

    Add test for filename in verbose output

--- test/integration/test.ts
+++ test/integration/test.ts
@@ -1,8 +1,16 @@
 const shelltest = require("shelltest");
 
+const mort = "./dist/src/main.js";
 test("it warns us if no file has been passed", done => {
     shelltest()
-        .cmd("./dist/src/main.js")
-    .expect("stdout", "Please supply a css file\n")
-    .end(done);
+        .cmd(mort)
+        .expect("stdout", "Please supply a css file\n")
+        .end(done);
+});
+
+test("it displays the filename if verbose is 1 or higher", done => {
+    shelltest()
+        .cmd(`${mort} -v -f ./test/fixtures/no-usages.css`)
+        .expect("stdout", /test\/fixtures\/no-usages\.css/)
+        .end(done);
 });
