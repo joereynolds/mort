bc9abb025297224aef1f5b6ad09158d0453259e9
Joe Reynolds <joereynolds952@gmail.com>
2018-05-27 15:57:37 +0100

    Delete rogue .js file

--- test/integration/test.js
+++
@@ -1,8 +0,0 @@
-var shelltest = require("shelltest");
-var mort = "./dist/src/main.js";
-test("it displays the filename if verbose is 1 or higher", function (done) {
-    shelltest()
-        .cmd(mort + " -v -f ./test/fixtures/no-usages.css")
-        .expect("stdout", /test\/fixtures\/no-usages\.css/)
-        .end(done);
-});
